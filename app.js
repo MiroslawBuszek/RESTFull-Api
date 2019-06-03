37c0ac092a9af2149f4e322701ca0653abe08b24
Mirosław Buszek <zoowett@gmail.com>
2019-06-03 22:49:32 +0200

    Create server with nodejs and expressjs

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my app')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
